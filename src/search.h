f164471dbcb5add974b95bf0c246a326b86e7d63
abulmo <abulmo@gmail.com>
2024-09-01 00:20:10 +0200

    merge with edax 4.5.2

--- src/search.h
+++ src/search.h
@@ -3,14 +3,15 @@
  *
  * Search's header file.
  *
- * @date 1998 - 2017
+ * @date 1998 - 2023
  * @author Richard Delorme
- * @version 4.4
+ * @version 4.5
  */
 
 #ifndef EDAX_SEARCH_H
 #define EDAX_SEARCH_H
 
+#include "bit.h"
 #include "board.h"
 #include "const.h"
 #include "empty.h"
@@ -44,7 +45,7 @@ typedef struct Result {
 	int move;                    /**< best move found */
 	int score;                   /**< best score */
 	Bound bound[BOARD_SIZE + 2]; /**< score bounds / move */
-	Line pv[1];                  /**< principal variation */
+	Line pv;                     /**< principal variation */
 	long long time;              /**< searched time */
 	unsigned long long n_nodes;  /**< searched node count */
 	bool book_move;              /**< book move origin */
@@ -55,25 +56,28 @@ typedef struct Result {
 
 /** levels */
 extern struct Level {
-	int depth;         /** search depth */
-	int selectivity;   /** search selectivity level */
+	unsigned char depth;         /** search depth */
+	unsigned char selectivity;   /** search selectivity level */
 } LEVEL[61][61];
 
 
 /** search stare */
 typedef struct Search {
-	Board board[1];                               /**< othello board */
+	Board board;                                  /**< othello board (16) */
+
+	volatile unsigned long long n_nodes;          /**< node counter (8) */
+	volatile unsigned long long child_nodes;      /**< node counter (8) */
+
+	Eval eval;                                    /**< eval */
+
 	SquareList empties[BOARD_SIZE + 2];           /**< list of empty squares */
-	SquareList *x_to_empties[BOARD_SIZE + 2];     /**< link position to the list */
-	int n_empties;                                /**< number of empty squares */
 	int player;                                   /**< player color */
 	int id;                                       /**< search id */
 
-	HashTable hash_table[1];                      /**< hashtable */
-	HashTable pv_table[1];                        /**< hashtable for the pv */
-	HashTable shallow_table[1];                   /**< hashtable for short search */
-	Eval eval[1];                                 /**< eval */
-	Random random[1];                             /**< random generator */
+	HashTable hash_table;                         /**< hashtable */
+	HashTable pv_table;                           /**< hashtable for the pv */
+	HashTable shallow_table;                      /**< hashtable for short search */
+	Random random;                                /**< random generator */
 
 	struct TaskStack *tasks;                      /**< available task queue */
 	struct Task *task;                            /**< search task */
@@ -86,7 +90,6 @@ typedef struct Search {
 	int depth;                                    /**< depth level */
 	int selectivity;                              /**< selectivity level */
 	int probcut_level;                            /**< probcut recursivity level */
-	unsigned int parity;                          /**< parity */
 	int depth_pv_extension;                       /**< depth for pv_extension */
 	volatile Stop stop;                           /**< thinking status */
 	bool allow_node_splitting;                    /**< allow parallelism */
@@ -98,7 +101,7 @@ typedef struct Search {
 		long long  mini;                          /**< minimal alloted time */
 		long long  maxi;                          /**< maximal alloted time */
 	} time;                                       /**< time */
-	MoveList movelist[1];                         /**< list of moves */
+	MoveList movelist;                            /**< list of moves */
 	int height;                                   /**< search height from root */
 	NodeType node_type[GAME_SIZE];                /**< node type (pv node, cut node, all node) */
 	Bound stability_bound;                        /**< score bounds according to stable squares */
@@ -114,26 +117,23 @@ typedef struct Search {
 		const char *separator;                    /**< separator for search output */
 		bool guess_pv;                            /**< guess PV (in cassio mode only) */
 		int multipv_depth;                        /**< multi PV depth */
-		int hash_size;                            /**< hashtable size */                    
+		int hash_size;                            /**< hashtable size */
 	} options;                                    /**< local (threadable) options. */
 
 	Result *result;                               /**< shared result */
 
 	void (*observer)(Result*);                    /**< call back function to print search result */
-
-	volatile unsigned long long n_nodes;          /**< node counter */
-	volatile unsigned long long child_nodes;      /**< node counter */
-
 } Search;
 
 struct Node;
 
-extern const int QUADRANT_ID[];
+extern const unsigned char QUADRANT_ID[];
+extern const unsigned long long quadrant_mask[];
 extern const Selectivity selectivity_table[];
 extern const int NO_SELECTIVITY;
-extern const int NWS_STABILITY_THRESHOLD[];
-extern const int PVS_STABILITY_THRESHOLD[];
-extern const int SQUARE_TYPE[];
+// extern const signed char NWS_STABILITY_THRESHOLD[];
+extern const signed char PVS_STABILITY_THRESHOLD[];
+extern const unsigned char SQUARE_TYPE[];
 
 /* function definition */
 void search_global_init(void);
@@ -159,13 +159,13 @@ void search_set_task_number(Search*, const int);
 
 void search_swap_parity(Search*, const int);
 void search_get_movelist(const Search*, MoveList*);
-void search_update_endgame(Search*, const Move*);
-void search_restore_endgame(Search*, const Move*);
-void search_pass_endgame(Search*);
+// void search_update_endgame(Search*, const Move*);
+// void search_restore_endgame(Search*, const Move*);
+// void search_pass_endgame(Search*);
 void search_update_midgame(Search*, const Move*);
-void search_restore_midgame(Search*, const Move*);
-void search_update_pass_midgame(Search*);
-void search_restore_pass_midgame(Search*);
+void search_restore_midgame(Search*, int, const Eval*);
+void search_update_pass_midgame(Search*, Eval*);
+void search_restore_pass_midgame(Search*, const Eval*);
 long long search_clock(Search*);
 long long search_time(Search*);
 unsigned long long search_count_nodes(Search*);
@@ -175,30 +175,31 @@ int get_pv_extension(const int, const int);
 
 void result_print(Result*, FILE*);
 
-bool search_SC_PVS(Search*, volatile int*, volatile int*, int*);
+// bool search_SC_PVS(Search*, int*, int*, int*);
 bool search_SC_NWS(Search*, const int, int*);
-bool search_TC_PVS(HashData*, const int, const int, volatile int*, volatile int*, int*);
+bool search_SC_NWS_4(Search*, const int, int*);
+// bool search_TC_PVS(HashData*, const int, const int, int*, int*, int*);
 bool search_TC_NWS(HashData*, const int, const int, const int, int*);
-bool search_ETC_PVS(Search*, MoveList*, unsigned long long, const int, const int, volatile int*, volatile int*, int*);
+// bool search_ETC_PVS(Search*, MoveList*, unsigned long long, const int, const int, int*, int*, int*);
 bool search_ETC_NWS(Search*, MoveList*, unsigned long long, const int, const int, const int, int*);
 
 NodeType next_node_type(const NodeType parent, const bool first_move);
 
 int search_solve(const Search*);
 int search_solve_0(const Search*);
-extern int board_score_1(const Board*, const int, const int);
+extern int board_score_1(const unsigned long long, const int, const int);
 int NWS_endgame(Search*, const int);
 
 int search_eval_0(Search*);
-int search_eval_1(Search*, const int, int);
-int search_eval_2(Search*, int, const int);
+int search_eval_1(Search*, int, int, unsigned long long);
+int search_eval_2(Search*, int, int, unsigned long long);
 int NWS_midgame(Search*, const int, int, struct Node*);
 int PVS_midgame(Search*, const int, const int, int, struct Node*);
-int NWS_shallow(Search*, const int, int, HashTable*);
+// static int NWS_shallow(Search*, const int, int, HashTable*);
 int PVS_shallow(Search*, int, int, int);
 
 bool is_pv_ok(Search*, int, int);
-void record_best_move(Search*, const Board*, const Move*, const int, const int, const int);
+void record_best_move(Search*, const Move*, const int, const int, const int);
 int PVS_root(Search*, const int, const int, const int);
 int aspiration_search(Search*, int, int, const int, int);
 void iterative_deepening(Search*, int, int);
@@ -219,6 +220,37 @@ void pv_debug(Search*, const Move*, FILE*);
 int search_get_pv_cost(Search*);
 void show_current_move(FILE *f, Search*, const Move*, const int, const int, const bool);
 int search_bound(const Search*, int);
+
+#if defined(hasSSE2) || defined(__ARM_NEON) || defined(USE_GAS_MMX) || defined(USE_MSVC_X86) || defined(ANDROID)
+  #ifdef __AVX2__
+	#define	mm_malloc(s)	_mm_malloc((s), 32)
+	#define	mm_free(p)	_mm_free(p)
+  #elif defined(hasSSE2) && !defined(ANDROID)
+	#define	mm_malloc(s)	_mm_malloc((s), 16)
+	#define	mm_free(p)	_mm_free(p)
+  #elif defined(_MSC_VER)
+	#define	mm_malloc(s)	_aligned_malloc((s), 16)
+	#define	mm_free(p)	_aligned_free(p)
+  #else
+	static inline void *mm_malloc(size_t s) {
+		void *p = malloc(s + 16 + sizeof(void *));
+		if (!p) return p;
+		void **q = (void **)(((size_t) p + 15 + sizeof(void *)) & -16);
+		*(q - 1) = p;
+		return (void *) q;
+	}
+	#define mm_free(p)	free(*((void **)(p) - 1));
+  #endif
+#else
+	#define	mm_malloc(s)	malloc(s)
+	#define	mm_free(p)	free(p)
+#endif
+
+#ifdef hasSSE2	// search->board is aligned
+	#define	search_pass(search)	_mm_store_si128((__m128i *) &(search)->board, _mm_shuffle_epi32(*(__m128i *) &(search)->board, 0x4e))
+#else
+	#define	search_pass(search)	board_pass(&(search)->board)
+#endif
 
 #endif
 
