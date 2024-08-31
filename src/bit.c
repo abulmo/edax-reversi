803bb9610882fee18c3c4ae14345ec2500cd8c73
abulmo <abulmo@gmail.com>
2024-09-01 00:42:55 +0200

    Update last_bit from chessprogramming wiki

--- src/bit.c
+++ src/bit.c
@@ -583,7 +583,9 @@ int last_bit(unsigned long long b)
 		13, 18,  8, 12,  7,  6,  5, 63
 =======
 #elif defined(HAS_CPU_64)
+	// https://www.chessprogramming.org/BitScan#De_Bruijn_Multiplication_2
 	static const unsigned char magic[64] = {
+<<<<<<< HEAD
 		63, 0, 58, 1, 59, 47, 53, 2,
 		60, 39, 48, 27, 54, 33, 42, 3,
 		61, 51, 37, 40, 49, 18, 28, 20,
@@ -593,6 +595,16 @@ int last_bit(unsigned long long b)
 		56, 45, 25, 31, 35, 16, 9, 12,
 		44, 24, 15, 8, 23, 7, 6, 5
 >>>>>>> 1b29848 (fix & optimize 32 bit build; other minor mods)
+=======
+		 0, 47,  1, 56, 48, 27,  2, 60,
+		57, 49, 41, 37, 28, 16,  3, 61,
+		54, 58, 35, 52, 50, 42, 21, 44,
+		38, 32, 29, 23, 17, 11,  4, 62,
+		46, 55, 26, 59, 40, 36, 15, 53,
+		34, 51, 20, 43, 31, 22, 10, 45,
+		25, 39, 14, 33, 19, 30,  9, 24,
+		13, 18,  8, 12,  7,  6,  5, 63
+>>>>>>> 13d6004 (Update last_bit from chessprogramming wiki)
 	};
 
 	b |= b >> 1;
